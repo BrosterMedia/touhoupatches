8b04e1044dec09c378f0f74a7cdae576a23df7f2
Broster <12662000+BrosterMedia@users.noreply.github.com>
2023-05-11 00:15:20 +0200

    stuff

---
+++ playable_seiran/patch.js
@@ -0,0 +1,13 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+		"thpatch/lang_en"
+	],
+	"id": "playable_seiran",
+	"servers": [
+		"https://raw.githubusercontent.com/Zrrg/UnKnwn/master/untanned_cirno/"
+	],
+	"title": "PlayableSeiran"
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "broster#0001",
+	"id": "broster",
+	"patches": {
+		"playable_seiran": "PlayableSeiran: Replaces reimu's sprites with Seiran (Th17)"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/Zrrg/UnKnwn/master/"
+	],
+	"title": "Broster Thcrap Repository"
+}
